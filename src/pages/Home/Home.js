c574ff9c517895a5a7d492481839b61a0ede9fdd
oreva <orevaawusi@gmail.com>
2024-10-18 03:15:36 +0100

    Logo Changed and backend url changed to point directly to render

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { RiProductHuntLine } from "react-icons/ri";
+import { CiBitcoin } from "react-icons/ci";
 import { Link } from "react-router-dom";
 import "./Home.scss";
 import heroImg from "../../assets/inv-img.png";
@@ -10,7 +10,7 @@ const Home = () => {
     <div className="home">
       <nav className="container --flex-between ">
         <div className="logo">
-          <RiProductHuntLine size={35} />
+          <CiBitcoin size={45} />Vent
         </div>
 
         <ul className="home-links">
