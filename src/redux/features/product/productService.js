43d7ab05299cbfbad1a1dd72a1b9126858a03170
oreva <orevaawusi@gmail.com>
2024-10-18 03:25:29 +0100

    url change

--- src/redux/features/product/productService.js
+++ src/redux/features/product/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "https://bvent-app-api.onrender.com";
+const BACKEND_URL = "https://bvent-api.onrender.com";
 
 const API_URL = `${BACKEND_URL}/api/products/`;
 
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-export const BACKEND_URL = "https://bvent-app-api.onrender.com";
+export const BACKEND_URL = "https://bvent-api.onrender.com";
 
 export const validateEmail = (email) => {
   return email.match(
